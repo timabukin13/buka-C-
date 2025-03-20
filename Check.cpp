67c5d99884afb65302fc1b06ccb64c103ffceb83
buka <161470535+timabukin13@users.noreply.github.com>
2025-03-20 20:46:28 +0300

    Create Check.cpp

---
+++ Check.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool OpCheck(char a){
+    if(a == '*' || a == '/' || a == '+' || a == '-'){return true;}
+    return false;
+}
+bool IntCheck(char a){
+    if(a == '1' || a == '4' || a == '2' || a == '3' || a == '5' || a == '6' || a == '7' || a == '8' || a == '9'){return true;}
+    return false;
+}
+bool Check(string str){
+    int state = 0;
+    for(int i = 0; i < str.length(); i ++){
+        char g = str[i];
+        if(state == 0 && g == '('){state = 1;}
+        else if(state == 0 && IntCheck(g)){state = 2;}
+        else if(state == 1 && (IntCheck(g) || g =='0')){state = 2;}
+        else if(state == 2 && (IntCheck(g) || g =='0')){state = 2;}
+        else if(state == 2 && OpCheck(g)){state = 3;}
+        else if(state == 3 && g == '('){state = 1;}
+        else if(state == 3 && IntCheck(g)){state = 4;}
+        else if(state == 4 && (IntCheck(g) || g =='0')){state = 4;}
+        else if(state == 4 && OpCheck(g)){state = 3;}
+        else if(state == 4 && g == ')'){state = 5;}
+        else if(state == 5 && OpCheck(g)){state = 3;}
+        else{cout<<"No";return false;}
+    
+    }
+    if(state == 5 || state == 4){cout<<"Yes";return true;}
+    else{cout<<"No";return false;}
+    
+}
+int main() {
+	string str = "";
+	cin >> str;
+	bool res = Check(str);
+	
+	return 0;
+
+}
