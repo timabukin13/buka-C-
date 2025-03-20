884ac1bd905d6f1634b63c94ee7bb771bff4c45d
buka <161470535+timabukin13@users.noreply.github.com>
2025-03-20 22:36:55 +0300

    Update sfml.cpp

--- sfml.cpp
+++ sfml.cpp
@@ -1,20 +1,17 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(1000,1000), "Graphic1.1");
-    while(window.isOpen())
-    {
+int main(){
+    sf::RenderWindow window(sf::VideoMode(1920,1080), "Window");
+    while(window.isOpen()){
         sf:: Event event;
-        while(window.pollEvent(event))
-        {
-            if(event.type == sf:: Event:: Closed)
-            {
+        while(window.pollEvent(event)){
+            if(event.type == sf:: Event:: Closed){
                 window.close();
             }
         }
         window.clear(sf::Color::Black);
         window.display();
     }
+    return 0;
 }
