ac4e5c87d77779af9d268268196a2c26b20e7abe
buka <161470535+timabukin13@users.noreply.github.com>
2025-05-23 20:39:17 +0300

    Add files via upload

---
+++ hafman_out.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <bitset>
+
+
+using namespace std;
+
+
+vector<char> conv(const string& bin_str) {
+    vector<char> result;
+    string temp = bin_str;
+    
+    while (temp.length() % 8 != 0) {
+        temp += '0';
+    }
+    
+    for (size_t i = 0; i < temp.size(); i += 8) {
+        string byte_str = temp.substr(i, 8);
+        bitset<8> bits(byte_str);
+        result.push_back(static_cast<char>(bits.to_ulong()));
+    }
+    
+    return result;
+}
+
+int main() {
+    string input;
+    cout << "Введите строку: ";
+    cin >> input;
+    vector<char> bytes = conv(input);
+    cout << "\n В виде символов:     ";
+    for (auto b : bytes){
+    cout << static_cast<char>(b);
+    }
+    return 0;
+}
